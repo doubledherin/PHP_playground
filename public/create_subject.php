7ac844d64e071a5499da36f7f194546e703c8ed0
Wendy Dherin <wendydherin@gmail.com>
2016-05-14 09:49:21 -0700

    Use session ID to pass subject-creation message.

--- public/create_subject.php
+++ public/create_subject.php
@@ -1,3 +1,4 @@
+<?php require_once("../includes/session.php"); ?>
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 <?php
@@ -18,11 +19,11 @@
 
 			if ($result) {
 				// Success
-				echo"Subject created.";
+				$_SESSION["message"] = "Subject created!";
 				redirect_to("manage_content.php");
 			} else {
 				// Failure
-				echo "Subject creation failed.";
+				$_SESSION["message"] = "Subject creation failed!";
 				redirect_to("new_subject.php");
 			}
 	} else {
