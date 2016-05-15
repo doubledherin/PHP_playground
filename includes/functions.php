27dfc117301d846976cea009e9a975a70242cb53
Wendy Dherin <wendydherin@gmail.com>
2016-05-15 16:51:17 -0700

    Prevent users from accessing pages set to not visible in the url.

--- includes/functions.php
+++ includes/functions.php
@@ -78,7 +78,7 @@
 		return $subject_id;
 	}
 
-	function find_subject_by_id($subject_id) {
+	function find_subject_by_id($subject_id, $public=true) {
 		global $connection;
 		
 		$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
@@ -86,6 +86,9 @@
 		$query  = "SELECT * ";
 		$query .= "FROM subjects ";
 		$query .= "WHERE id = {$safe_subject_id} ";
+		if ($public) {
+			$query .= "AND visible = 1 ";
+		}
 		$query .= "LIMIT 1";
 		$subject_set = mysqli_query($connection, $query);
 		confirm_query($subject_set);
@@ -95,7 +98,7 @@
 			return null;
 		}
 	}
-	function find_page_by_id($page_id) {
+	function find_page_by_id($page_id, $public=true) {
 		global $connection;
 		
 		$safe_page_id = mysqli_real_escape_string($connection, $page_id);
@@ -103,6 +106,9 @@
 		$query  = "SELECT * ";
 		$query .= "FROM pages ";
 		$query .= "WHERE id = {$safe_page_id} ";
+		if ($public) {
+			$query .= "AND visible = 1 ";
+		}
 		$query .= "LIMIT 1";
 		$page_set = mysqli_query($connection, $query);
 		confirm_query($page_set);
@@ -118,15 +124,15 @@
 		global $current_page;
 		
 		if (isset($_GET["subject"])) {
-			$current_subject = find_subject_by_id($_GET["subject"]);
-			if ($public) {
+			$current_subject = find_subject_by_id($_GET["subject"], $public);
+			if ($current_subject && $public) {
 				$current_page = find_default_page_for_subject($current_subject["id"]);	
 			} else {
 				$current_page = null;
 			}
 		} elseif (isset($_GET["page"])) {
 			$current_subject = null;
-			$current_page = find_page_by_id($_GET["page"]);
+			$current_page = find_page_by_id($_GET["page"], $public);
 		} else {
 			$current_subject = null;
 			$current_page = null;
